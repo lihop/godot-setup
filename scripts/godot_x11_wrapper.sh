1a5160836c68751f0d1cf5b090dd4d532d21d4d3
Leroy Hopson <git@leroy.geek.nz>
2021-07-24 11:36:49 +0700

    Working for Ubuntu 64bit

---
+++ scripts/godot_x11_wrapper.sh
@@ -0,0 +1,17 @@
+set -e
+# Starts godot with X11 Display using Xvfb and dummy sound device with pulseaudio.
+# Also sets locale if not already set.
+
+# Set locale to 'en' if locale is not already set.
+# Godot will fallback to this locale anyway and it
+# prevents an error message being printed to console.
+if [ "$LANG" == "C.UTF-8" ]; then LANG=en; fi
+
+# Start dummy sound device.
+pulseaudio --check || pulseaudio -D
+
+# Running godot with X11 Display.
+xvfb-run --auto-servernum GODOT_EXECUTABLE "$@"
+
+# Cleanup.
+pulseaudio -k
